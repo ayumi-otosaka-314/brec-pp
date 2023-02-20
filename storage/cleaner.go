fdfbcd59f5d1c53414f046dc8e01702bb1d96a27
乙坂あゆみ <ayumi.o@aol.com>
2023-02-20 20:58:25 +0800

    refactor(storage cleaner): simplified channel usage

--- storage/cleaner.go
+++ storage/cleaner.go
@@ -8,16 +8,16 @@ import (
 
 type Cleaner interface {
 	GetAvailableCapacity() (uint64, error)
-	GetRemovables(context.Context) (<-chan Removable, error)
+	GetRemovables(context.Context) (<-chan DoRemove, error)
 }
 
-type Removable interface {
-	RemoveResult() <-chan error
-	OccupiedSize() uint64
-}
+// DoRemove is the action to actually remove removable.
+// It would return the space cleared in byte count, and error if any during cleaning.
+type DoRemove func() (uint64, error)
 
 func EnsureCapacity(targetCapacity uint64, cleaner Cleaner) error {
-	for {
+	const allowedIterations = 5
+	for i := 0; i < allowedIterations; i++ {
 		availCapacity, err := cleaner.GetAvailableCapacity()
 		if err != nil {
 			return errors.Wrap(err, "unable to check available bytes")
@@ -31,6 +31,7 @@ func EnsureCapacity(targetCapacity uint64, cleaner Cleaner) error {
 			return err
 		}
 	}
+	return errors.Errorf("unable to ensure capacity in [%d] iterations", allowedIterations)
 }
 
 func doEnsureCapacity(cleanTarget uint64, cleaner Cleaner) error {
@@ -42,12 +43,13 @@ func doEnsureCapacity(cleanTarget uint64, cleaner Cleaner) error {
 		return errors.Wrap(err, "unable to get removables")
 	}
 
-	for removable := range removables {
-		if err := <-removable.RemoveResult(); err != nil {
+	for remove := range removables {
+		clearedSize, err := remove()
+		if err != nil {
 			return errors.Wrap(err, "error removing object; stopping")
 		}
 
-		cleanTarget -= removable.OccupiedSize()
+		cleanTarget -= clearedSize
 		if cleanTarget <= 0 {
 			return nil
 		}
