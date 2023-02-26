583fd9e2c65d09b3972faaf660f9dad9d2b15524
乙坂あゆみ <ayumi.o@aol.com>
2023-02-26 15:03:19 +0800

    refactor: remove unused interface

--- streamer/meta_registry.go
+++
@@ -1,6 +0,0 @@
-package streamer
-
-type MetaRegistry interface {
-	GetAvatar(streamerUID uint64, sessionID string) (string, error)
-	GetThumbnail(roomID uint64, sessionID string) (string, error)
-}
